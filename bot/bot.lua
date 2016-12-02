98fc34b05409b7d6497703d803e0e36eca77e2dd
climaxit <climaxit3@gmail.com>
2016-12-02 13:31:16 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -263,9 +263,10 @@ function create_config( )
     "Block",
     "InGroups",
     },
-    sudo_users = {56693692,},--Sudo users
+    sudo_users = {275387751},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[
+		climaxit
 ]],
     help_text_realm = [[
 
